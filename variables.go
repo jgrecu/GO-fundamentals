db054c4e88be9f78801a2bd9082cf3ade60cb49a
Jeremy Grecu <jgrecu@gmail.com>
2020-12-12 20:16:33 +0000

    vars and const

--- variables.go
+++ variables.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"reflect"
 )
 
 //var (
@@ -13,8 +12,19 @@ import (
 
 func main() {
 	name	:= "Nigel"
-	//course	:= "Docker Deep Dive"
-	module	:= 3.2
-	fmt.Println("Name is ", name, "and is of type", reflect.TypeOf(name))
-	fmt.Println("Module is ", module, "and is of type", reflect.TypeOf(module))
+	course	:= "Docker Deep Dive"
+
+	fmt.Println("\nHi", name, "you;re currently watching",
+		course)
+
+	changeCourse(course)
+	fmt.Println("\nYou are watching course", course)
+}
+
+func changeCourse(course string) string {
+	course = "First Look: Native Docker Clustering"
+
+	fmt.Println("\nTrying to change your course to", course)
+
+	return course
 }
